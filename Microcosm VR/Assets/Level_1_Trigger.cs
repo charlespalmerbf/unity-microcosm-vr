45a5c2e0051a38bf008a8c505c7aca54e069bfb0
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-02-25 01:09:39 +0000

    Create Level_1_Trigger.cs
    Creating a trigger that teleports the player from the end of the Tutorial to the start of Level 1

---
+++ Microcosm VR/Assets/Level_1_Trigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Level_1_Trigger : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+
+    {
+    	SceneManager.LoadScene(1);
+    }
+
+  
+}
