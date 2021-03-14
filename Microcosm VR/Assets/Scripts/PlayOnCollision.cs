4c5181e2e2a39e5ce3e3830955635dc0f55855dd
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-03-14 21:14:38 +0000

    Creating PlayOnCollision Script
    Creating a script that allows the application to play audio when a user grabs onto an object. This has been used for the narration of the game.

---
+++ Microcosm VR/Assets/Scripts/PlayOnCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.Audio;
+
+
+public class PlayOnCollision : MonoBehaviour
+
+{
+
+    private AudioSource audioSource;
+
+    void OnCollisionEnter(Collision collision)
+
+    {
+
+        audioSource.Play();
+
+    }
+    
+}
